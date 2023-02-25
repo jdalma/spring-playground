91394cffbba6fc2e422150976188ea1914fe6918
정현준 <jeongdalma@gmail.com>
2023-02-25 17:26:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Kotlin으로 Mybatis와 JPA 사용해보기
+
+- Mybatis는 [예제](https://github.com/jeffgbutler/mybatis-kotlin-examples)를 참고하여 작성
---
+++ src/main/kotlin/com/example/dataaccess/DataAccessApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dataaccess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataAccessApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataAccessApplication>(*args)
+}
